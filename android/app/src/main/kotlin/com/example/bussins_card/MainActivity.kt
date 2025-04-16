67549847256e0833b8e44998da5feac204e50a9e
MohammedZakiBinMahfood <mohammadbinmahfood@gmail.com>
2025-04-16 23:43:29 +0300

    initial committ

---
+++ android/app/src/main/kotlin/com/example/bussins_card/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.bussins_card
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,150 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const  BusienessCardApp());
+}
+
+class BusienessCardApp extends StatelessWidget {
+   const BusienessCardApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: const Color(0xFF2B475E),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const CircleAvatar(
+              radius: 110,
+              backgroundColor: Colors.white,
+              child: CircleAvatar(
+                radius: 125,
+                backgroundImage: AssetImage('images/20230423.jpg'),
+              ),
+            ),
+            const Text(
+              'Mohammed Zaki',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 28,
+                fontFamily: 'Playwrite England SemiJoined',
+              ),
+            ),
+            const Text(
+              'Flutter developer',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 18,
+                fontFamily: 'Orbitron',
+              ),
+            ),
+            const Divider(
+              thickness: 1,
+              color: Colors.white,
+              indent: 60,
+              endIndent: 60,
+              height: 10,
+            ),
+            Card(
+              margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8)),
+              child: const ListTile(
+                leading: Icon(
+                  Icons.phone,
+                  color: Color(0xFF2B475E),
+                  size: 32,
+                ),
+                title: Text(
+                  '(+967) 770603656',
+                  style: TextStyle(
+                    fontSize: 23,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+            ),
+            Card(
+              margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8)),
+              child: const ListTile(
+                leading: Icon(
+                  Icons.email,
+                  color: Color(0xFF2B475E),
+                  size: 32,
+                ),
+                title: Text(
+                  'mohammadbinmahfood@gmail.com',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+            ),
+
+            // Container(
+            //   margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+
+            //   padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+            //   height: 65,
+            //   decoration: BoxDecoration(
+            //     borderRadius: BorderRadius.circular(8),
+            //     color: Colors.white,
+            //   ),
+            //   child: Row(
+            //     children: [
+            //       Icon(
+            //         Icons.phone,
+            //         color: Color(0xFF2B475E),
+            //         size: 32,
+            //       ),
+            //       Padding(
+            //         padding: const EdgeInsets.only(left: 16),
+            //         child: Text(
+            //           '(+967) 770603656',
+            //           style: TextStyle(
+            //             fontSize: 23,
+            //             color: Colors.black,
+            //           ),
+            //         ),
+            //       ),
+            //     ],
+            //   ),
+            // ),
+            // Container(
+            //   height: 65,
+            //   margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+            //   //padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+            //   decoration: BoxDecoration(
+            //     borderRadius: BorderRadius.circular(8),
+            //     color: Colors.white,
+            //   ),
+            //   child: Row(
+            //     children: [
+            //       Icon(
+            //         Icons.email,
+            //         color: Color(0xFF2B475E),
+            //         size: 32,
+            //       ),
+            //       Padding(
+            //         padding: const EdgeInsets.only(left: 16),
+            //         child: Text(
+            //           'mohammadbinmahfood@gmail.com',
+            //           style: TextStyle(
+            //             fontSize: 18,
+            //             color: Colors.black,
+            //           ),
+            //         ),
+            //       ),
+            //     ],
+            //   ),
+            // )
+          ],
+        ),
+      ),
+    );
+  }
+}
